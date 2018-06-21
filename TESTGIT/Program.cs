049b7e02e9e0eee498c565a7ac61bca65093884f
Raluca Covrig <raluca.covrig@endava.com>
2018-06-21 17:03:30 +0300

    test

--- TESTGIT/Program.cs
+++ TESTGIT/Program.cs
@@ -10,6 +10,9 @@ namespace TestGIT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2!");
+            Console.ReadLine();
         }
     }
 }

0746a55aa1304584fddda6207dba73871617682a
Raluca Covrig <raluca.covrig@endava.com>
2018-06-22 10:02:41 +0300

    testare commit

--- TESTGIT/Program.cs
+++ TESTGIT/Program.cs
@@ -13,6 +13,9 @@ namespace TestGIT
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World2!");
             Console.WriteLine("Hello World3!");
+            Console.WriteLine("Hello World4!");
+            Console.WriteLine("Hello World5!");
+            Console.WriteLine("Hello World6!");
             Console.ReadLine();
         }
     }
